15ee97217ea3964b2b9c10971dc9b1ef44f1466b
Santoshi Joshi <85894419+santoshi1396@users.noreply.github.com>
2021-07-29 17:04:14 +0530

    Static variable and method

---
+++ StaticDemo2.java
@@ -0,0 +1,13 @@
+class StaticDemo2{
+
+	static int x=50;
+	
+	static void display(){
+	System.out.println("Static Method");	
+	}
+	
+	public static void main(String args[]){
+	System.out.println(x);
+	display();
+	}
+}
